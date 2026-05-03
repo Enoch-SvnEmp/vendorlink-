59f70fa75a86ccb76d3905295113cc98412d1881
[Enoch Atikpo] <enochatikpo@gmail.com>
2026-05-03 01:12:36 -1200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res) => {
+//     res.send("VendorLink server running");
+// });
+
+// app.listen(3000, () => {
+//     console.log("Server running on port 3000");
+// });
+
+
+// new changes being made to the above code.........
+
 const express = require("express");
 const app = express();
 
+app.use(express.json()); // allows JSON data
+
 app.get("/", (req, res) => {
-    res.send("VendorLink server running");
+    res.send("VendorLink API running");
+});
+
+app.post("/add-product", (req, res) => {
+    const product = req.body;
+    console.log(product);
+    res.send("Product received");
 });
 
 app.listen(3000, () => {
